46004310316f4510ef804bd93075159d407d94a2
Vaibhav Gaje <vaibhavgaje96@gmail.com>
2021-10-17 19:38:58 +0530

    StringCalculator class done

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,23 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+       int sum=0;
+       if (input.length()==0) {
         return 0;
+        }
+       
+       else if (input.length()==1) {
+            int x =Integer.parseInt(input);
+           return x ;
+           } 
+       
+       else {
+           for(i=0;i<input.length();i+2) {
+               int x =Integer.parseInt(input.charAt(i));
+               sum=sum+x;
+           }
+           return sum;
+       }
     }
 
 }
